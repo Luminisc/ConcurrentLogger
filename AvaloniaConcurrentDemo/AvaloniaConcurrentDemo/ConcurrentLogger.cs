bf14f9a75935324e9caa05415b0247ec7b21e37a
Luminisc <luminsc@gmail.com>
2018-07-15 21:59:27 +0300

    Some coding

---
+++ AvaloniaConcurrentDemo/AvaloniaConcurrentDemo/ConcurrentLogger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaConcurrentDemo
+{
+    class ConcurrentLogger
+    {
+	    private bool _loggerEnabled = false;
+	    readonly ConcurrentQueue<string> _logQueue = new ConcurrentQueue<string>();
+	    private Task _loggingTask;
+
+		public ConcurrentLogger()
+	    {
+		    SetupLogger();
+		}
+
+	    public void PushMessage(string text)
+	    {
+			_logQueue.Enqueue(text);
+	    }
+
+	    private void SetupLogger()
+	    {
+		    _loggerEnabled = true;
+		    _loggingTask = new Task(() =>
+		    {
+			    using (Stream stream = new FileStream("./output.log", FileMode.OpenOrCreate))
+			    using (TextWriter tw = new StreamWriter(stream))
+			    {
+				    while (_loggerEnabled)
+				    {
+					    if (_logQueue.TryDequeue(out var text))
+					    {
+						    tw.WriteLine(text);
+					    }
+				    }
+			    }
+		    });
+		    _loggingTask.Start();
+	    }
+	}
+}
---
+++ AvaloniaConcurrentDemo/AvaloniaConcurrentDemo/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Linq;
+
+namespace AvaloniaConcurrentDemo
+{
+	public class MainWindow : Window
+	{
+		ConcurrentLogger logger = new ConcurrentLogger();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+#if DEBUG
+			this.AttachDevTools();
+#endif
+			DoJob();
+		}
+		
+		private void InitializeComponent()
+		{
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		public void DoJob()
+		{
+			new Task(() =>
+			{
+				var tasks = Enumerable.Range(0, 3).Select(t => Task.Factory.StartNew(() =>
+				{
+					for (int i = 0; i < 1000; i++)
+					{
+						logger.PushMessage("Hey listen!");
+					}
+				})).ToArray();
+
+				Task.WaitAll(tasks);
+			}).Start();
+		}
+	}
+}
