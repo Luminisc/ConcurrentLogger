0df4b31dfa9db04fe175f16dc62a9ea35bb06971
Alexander Romanenko <luminsc@gmail.com>
2018-08-08 21:01:11 +0300

    added gdal

--- AvaloniaMVVM/AvaloniaMVVM/Program.cs
+++ AvaloniaMVVM/AvaloniaMVVM/Program.cs
@@ -4,31 +4,40 @@ using Avalonia.Logging.Serilog;
 using AvaloniaMVVM.ViewModels;
 using AvaloniaMVVM.Views;
 using System.Runtime.InteropServices;
+using OSGeo.GDAL;
+using System.IO;
 
 namespace AvaloniaMVVM
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			double x = Add(5.0, 17.0);
-			Console.WriteLine($"5 + 17 = {x}");
-			Console.WriteLine($"Current folder: {Environment.CurrentDirectory}");
-			BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
-		}
+    class Program
+    {
+        public static readonly string relativePathToRoot = @"..\..\..\..\";
+        public static string picturePath = Path.Combine(relativePathToRoot, @"Pics\Data_Envi\samson_1.img");
+        
 
-		public static AppBuilder BuildAvaloniaApp()
-			=> AppBuilder.Configure<App>()
-				.UsePlatformDetect()
-				.UseReactiveUI()
-				.LogToDebug();
+        static void Main(string[] args)
+        {
+            Dataset dataset;
+            double x = Add(5.0, 17.0);
+            Console.WriteLine($"5 + 17 = {x}");
+            Console.WriteLine($"Current folder: {Environment.CurrentDirectory}");
+            Gdal.AllRegister();
+            dataset = Gdal.Open(picturePath, Access.GA_ReadOnly);
+            BuildAvaloniaApp().Start<MainWindow>(() => new MainWindowViewModel());
+        }
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .UseReactiveUI()
+                .LogToDebug();
 
 #if OS_LINUX
 		[DllImport(@"./CMakeLibrary.so", EntryPoint = "Add")]
 #endif
 #if OS_WINDOWS
-		[DllImport(@"./CMakeLibrary.dll", EntryPoint = "Add")]
+        [DllImport(@"./CMakeLibrary.dll", EntryPoint = "Add")]
 #endif
-		public static extern double Add(double x, double y);
-	}
+        public static extern double Add(double x, double y);
+    }
 }
